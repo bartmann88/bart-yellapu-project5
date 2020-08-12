c686dca7624a75cd3bda5bb62928f216aa99fcc3
Bart Yellapu <38868384+bartmann88@users.noreply.github.com>
2020-08-12 12:48:29 -0500

    Api linked/Card Component/states/CSS - JSx has to be debugged

--- src/App.js
+++ src/App.js
@@ -4,28 +4,82 @@ import HeroCard from "./HeroCard";
 import axios from "axios";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: "",
+      showCard: false,
+      name: "",
+      image: "",
+      strength: "",
+      intelligence: "",
+      speed: "",
+      durability: "",
+      power: "",
+      combat: "",
+      fullName: "",
+      alterEgo: "",
+      publisher: "",
+      alignment: "",
+      placeOfBirth: "",
+    };
 
-  // ComponentDidMount is used for fetching the api. 
-
-  componentDidMount(){
-    axios
-      .get("https://superheroapi.com/api/10158549574959596/search/batman")
-      .then((response) => {
-        console.log(response.data)
-      })
-  
-      
- 
+    // BInding function
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSearch = this.handleSearch.bind(this);
   }
+
+  handleChange(event) {
+    this.setState({ query: event.target.value });
+  }
+
+  handleSearch() {
+    this.setState({ showCard: true });
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://superheroapi.com/api/10158549574959596/search/${this.state.query}`
+      )
+      .then((response) =>
+        response.data.results.map((val) => {
+          console.log(val);
+        })
+      );
+  }
+
   render() {
     return (
-    <div className="App">
-    
-      <input type ="text" placeholder = "search by name" className = "searchBar" ></input>
-
-    </div>
-
-    )
+      <div className="App">
+        <input
+          type="text"
+          placeholder="search by name"
+          className="searchBar"
+          value={this.state.query}
+          onChange={this.handleChange}
+        ></input>
+        <button className="searchBtn" onClick={this.handleSearch}>
+          Search
+        </button>
+        <div className={this.state.showCard ? "show" : "hide"}>
+          <HeroCard
+            // Biography
+            image={this.state.image}
+            name="{this.state.name}"
+            fullName={this.state.fullName}
+            placeOfBirth={this.state.placeOfBirth}
+            alterEgo={this.state.alterEgo}
+            publisher={this.state.publisher}
+            alignment={this.state.alignment}
+            // Powerstats
+            intelligence={this.state.intelligence}
+            strength={this.state.strength}
+            speed={this.state.speed}
+            durability={this.state.durability}
+            power={this.state.power}
+            combat={this.state.combat}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/HeroCard.js
+++ src/HeroCard.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 
 class HeroCard extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="card">
+        <h2>{this.props.name}</h2>
 
-    render(
-        ){
+        <img src={this.props.image} alt={this.props.atl} />
 
-            return ( 
-            <h1>Hero Card Hi</h1>
-                    )
-        }
-   
+        <h3>Biography</h3>
+        <ul>
+          <li>{this.props.fullName}</li>
+          <li>{this.props.placeOfBirth}</li>
+          <li>{this.props.alterEgo}</li>
+          <li>{this.props.publisher}</li>
+          <li>{this.props.alignment}</li>
+        </ul>
+
+        <h3>PowerStats</h3>
+        <ul>
+          <li>{this.props.intelligence}</li>
+          <li>{this.props.strength}</li>
+          <li>{this.props.speed}</li>
+          <li>{this.props.durability}</li>
+          <li>{this.props.power}</li>
+          <li>{this.props.combat}</li>
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default HeroCard;
